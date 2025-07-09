4b82e0e1bcfaa64695c1d7f6fc4d1ec459fb7096
Meta <meta_apex@gmail.com>
2025-07-09 21:38:27 +0800

    add metric

---
+++ core/metric/metric.go
@@ -0,0 +1,20 @@
+package metric
+
+import "github.com/meta-apex/zenith/core/prometheus"
+
+// A VectorOpts is a general configuration.
+type VectorOpts struct {
+	Namespace string
+	Subsystem string
+	Name      string
+	Help      string
+	Labels    []string
+}
+
+func update(fn func()) {
+	if !prometheus.Enabled() {
+		return
+	}
+
+	fn()
+}
